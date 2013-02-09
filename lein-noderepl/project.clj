aac106607e6e30ed732c76a142a76d35a8222fe3
Bodil Stokke <bodil@bodil.tv>
2013-02-09 21:20:50 +0100

    Bump to 0.1.5. Get rid of duplicated Leinjacker code and broken cljsbuild dependencies.

--- lein-noderepl/project.clj
+++ lein-noderepl/project.clj
@@ -1,8 +1,8 @@
-(defproject org.bodil/lein-noderepl "0.1.4"
+(defproject org.bodil/lein-noderepl "0.1.5"
   :description "Leiningen plugin for launching a Node.js CLJS REPL"
   :url "https://github.com/bodil/cljs-noderepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [leinjacker "0.4.0"]]
+                 [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
--- lein-noderepl/src/leiningen/noderepl.clj
+++ lein-noderepl/src/leiningen/noderepl.clj
@@ -1,35 +1,8 @@
 (ns leiningen.noderepl
-  (:use [leinjacker.eval :only [eval-in-project]])
-  (:require [leiningen.cljsbuild.config :as config]
-            [leiningen.cljsbuild.subproject :as subproject]
-            [leiningen.core.main :as main]
+  (:use [leinjacker.eval :only [in-project]])
+  (:require [leiningen.core.main :as main]
             [leiningen.trampoline :as ltrampoline]
             [leinjacker.deps :as deps]))
-
-(defmacro in-project
-  ;; Copied from leinjacker HEAD, still missing from 0.4.0
-  {:arglists '([project ns-forms* bindings? body*])}
-  [project bindings & [form :as forms]]
-  (let [bindings (apply hash-map bindings)
-        [ns-forms forms] (if (= 'ns (first form))
-                           [(rest form) (rest forms)]
-                           [nil forms])
-        ns-forms (if (symbol? (first ns-forms))
-                   ns-forms
-                   (cons (gensym) ns-forms))
-        f `(fn [[~@(keys bindings)]] ~@forms)]
-    `(eval-in-project ~project
-                      (list 'do
-                            '(ns ~@ns-forms)
-                            (list '~f (list 'quote [~@(vals bindings)]))))))
-
-(defn cljsbuild-subproject-fixup [project]
-  (assoc project :dependencies (vec (:dependencies project))))
-
-(defn make-subproject [project crossover-path builds]
-  (let [subp (cljsbuild-subproject-fixup
-              (subproject/make-subproject-lein2 project crossover-path builds))]
-    (deps/add-if-missing subp ['org.bodil/cljs-noderepl "0.1.4"])))
 
 (defmacro require-trampoline [& forms]
   `(if ltrampoline/*trampoline?*
@@ -42,7 +15,7 @@
   "Launch a ClojureScript REPL on Node.js."
   [project & args]
   (require-trampoline
-   (let [{:keys [crossover-path builds]} (config/extract-options project)]
-     (in-project (make-subproject project crossover-path builds) []
+   (let [project (deps/add-if-missing project '[org.bodil/cljs-noderepl "0.1.5"])]
+     (in-project project []
                  (ns (:require [cljs.repl.node :as node]))
                  (node/run-node-repl)))))
