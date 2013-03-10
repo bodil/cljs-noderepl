28b9e74a8b98dccf29b23564569dbbdcd790fc67
Bodil Stokke <bodil@bodil.tv>
2013-03-10 18:10:11 +0100

    Bump to 0.1.8.

--- cljs-noderepl/project.clj
+++ cljs-noderepl/project.clj
@@ -1,10 +1,10 @@
-(defproject org.bodil/cljs-noderepl "0.1.7"
+(defproject org.bodil/cljs-noderepl "0.1.8"
   :description "Node.js REPL environment for Clojurescript"
   :url "https://github.com/bodil/cljs-noderepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [org.clojure/clojurescript "0.0-1576"]
+  :dependencies [[org.clojure/clojure "1.5.0"]
+                 [org.clojure/clojurescript "0.0-1586"]
                  [cheshire "5.0.1"]
                  [com.cemerick/piggieback "0.0.4"]]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
--- lein-noderepl/src/leiningen/noderepl.clj
+++ lein-noderepl/src/leiningen/noderepl.clj
@@ -15,7 +15,7 @@
   "Launch a ClojureScript REPL on Node.js."
   [project & args]
   (require-trampoline
-   (let [project (deps/add-if-missing project '[org.bodil/cljs-noderepl "0.1.7"])]
+   (let [project (deps/add-if-missing project '[org.bodil/cljs-noderepl "0.1.8"])]
      (in-project project []
                  (ns (:require [cljs.repl.node :as node]))
                  (node/run-node-repl)))))
