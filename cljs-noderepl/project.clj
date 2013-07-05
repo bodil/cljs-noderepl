2c53047bfb5cff7fba1fb49131298d1d13597b8a
Bodil Stokke <bodil@bodil.org>
2013-07-05 12:29:24 +0200

    Bump Clojure and Cheshire.

--- cljs-noderepl/project.clj
+++ cljs-noderepl/project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/bodil/cljs-noderepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-1586"]
-                 [cheshire "5.0.1"]
+                 [cheshire "5.2.0"]
                  [com.cemerick/piggieback "0.0.5"]]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
--- lein-noderepl/project.clj
+++ lein-noderepl/project.clj
@@ -3,6 +3,6 @@
   :url "https://github.com/bodil/cljs-noderepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
                  [leinjacker "0.4.1"]]
   :eval-in-leiningen true)
