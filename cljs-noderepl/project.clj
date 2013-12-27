4740d6361f220a9861088f11c31b0d8abd72f8b6
Daniel Zacarias <dzacarias@me.com>
2013-12-27 00:16:20 +0000

    Bump piggieback version to 0.1.2

--- cljs-noderepl/project.clj
+++ cljs-noderepl/project.clj
@@ -5,5 +5,5 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [cheshire "5.2.0"]
-                 [com.cemerick/piggieback "0.0.5"]]
+                 [com.cemerick/piggieback "0.1.2"]]
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]})
