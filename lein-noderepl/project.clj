cbf5d0689726891a40ceeffdc6b6b2d9ff2abcd0
Bodil Stokke <bodil@bodil.org>
2014-02-09 00:48:01 +0000

    Tag for 0.1.11 release.

--- lein-noderepl/project.clj
+++ lein-noderepl/project.clj
@@ -1,8 +1,8 @@
-(defproject org.bodil/lein-noderepl "0.1.11-SNAPSHOT"
+(defproject org.bodil/lein-noderepl "0.1.11"
   :description "Leiningen plugin for launching a Node.js CLJS REPL"
   :url "https://github.com/bodil/cljs-noderepl"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [leinjacker "0.4.1"]]
+                 [leinjacker "0.4.2"]]
   :eval-in-leiningen true)
